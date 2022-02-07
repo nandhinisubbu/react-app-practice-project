cae19046cf39aeb58f2c07e7462d0cc6bf115c76
nandhini.subramani <nandhini.subramani@thoughtworks.com>
2022-02-07 12:42:50 +0530

    Creating basic structure of react app

---
+++ my-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const element = React.createElement('h1',null,"Hello World!!");
+ReactDom.render(element,document.getElementById('root'))
