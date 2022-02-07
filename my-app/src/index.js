5c913bd2c6753a7f2bc1678e13cc77bb3424a030
nandhini.subramani <nandhini.subramani@thoughtworks.com>
2022-02-07 14:45:55 +0530

    Try Component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDom from "react-dom";
 
 const users = ['Tom','Jerry','Micky'];
@@ -6,12 +6,33 @@ const users = ['Tom','Jerry','Micky'];
 //     users.map((user,index) => React.createElement('li',{key : index},user))
 // );
 
-const element =
-    <div>
-        <h1>Users</h1>
-        <ol>
-    {users.map((user,index) => <li key = {index}> {user} </li>)}
-    </ol>
-    </div>
 
-ReactDom.render(element,document.getElementById('root'))
+
+
+class List extends Component{
+    render(){
+        return (<ol>
+                {users.map((user,index) => <li key = {index}> {user} </li>)}
+             </ol>)
+    }
+
+}
+
+class Title extends Component{
+    render(){
+        return (<div>
+        <h1>Users</h1>
+    </div>)
+    }
+}
+
+class Main extends Component{
+    render(){
+        return <div>
+                <Title/>
+                <List/>
+            </div>
+    }
+}
+
+ReactDom.render(<Main/>,document.getElementById('root'))
