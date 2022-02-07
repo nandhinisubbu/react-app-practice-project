652ce506aa6625b0d63b353b82cb361f77a3fb77
nandhini.subramani <nandhini.subramani@thoughtworks.com>
2022-02-07 13:13:34 +0530

    Try JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const users = ['Tom','Jerry','Micky'];
-const element = React.createElement('h1',null,
-    users.map((user,index) => React.createElement('li',{key : index},user))
-);
+// const element = React.createElement('h1',null,
+//     users.map((user,index) => React.createElement('li',{key : index},user))
+// );
+
+const element =
+    <div>
+        <h1>Users</h1>
+        <ol>
+    {users.map((user,index) => <li key = {index}> {user} </li>)}
+    </ol>
+    </div>
 
 ReactDom.render(element,document.getElementById('root'))
